ff6898eb4d4aad8fd79184c8973c8780a43aeafe
Banner Ironxi <bannerironxi@foxmail.com>
2019-12-02 00:10:26 +0800

    spring batch:chapter4,spilt

---
+++ splits/src/test/java/io/spring/batch/splits/SplitsApplicationTests.java
@@ -0,0 +1,13 @@
+package io.spring.batch.splits;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SplitsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
