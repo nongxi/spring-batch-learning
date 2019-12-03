7111b6e12635e07144403da014c796cb5f8fb737
Banner Ironxi <bannerironxi@foxmail.com>
2019-12-04 01:20:06 +0800

    spring batch:missed file

---
+++ nestedJob/src/test/java/io/spring/batch/nestedJob/NestedJobApplicationTests.java
@@ -0,0 +1,13 @@
+package io.spring.batch.nestedJob;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NestedJobApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
