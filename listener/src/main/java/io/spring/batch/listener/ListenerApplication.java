8acafebf0dd58cf3ac45c7734e7d547429ae85d8
Banner Ironxi <bannerironxi@foxmail.com>
2019-12-08 01:10:44 +0800

    spring batch:chaprer 7,listener

---
+++ listener/src/main/java/io/spring/batch/listener/ListenerApplication.java
@@ -0,0 +1,13 @@
+package io.spring.batch.listener;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListenerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ListenerApplication.class, args);
+	}
+
+}
