68152c49c1d75da00f8cd6694cf25c661d053b40
Banner Ironxi <bannerironxi@foxmail.com>
2019-12-10 02:01:03 +0800

    spring batch:chatper 8,job parameters

---
+++ jobParameters/src/main/java/io/spring/batch/jobParameters/configuration/JobParametersConfiguration.java
@@ -0,0 +1,49 @@
+package io.spring.batch.jobParameters.configuration;
+
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.StepContribution;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.JobScope;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepScope;
+import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class JobParametersConfiguration {
+
+    @Autowired
+    private JobBuilderFactory jobBuilderFactory;
+
+    @Autowired
+    private StepBuilderFactory stepBuilderFactory;
+
+    @Bean
+    @StepScope
+    public Tasklet helloWorldTasklet(@Value("#{jobParameters['message']}") String message){
+        return (stepContribution,chunkContext) ->{
+          System.out.println("message");
+          return RepeatStatus.FINISHED;
+        };
+    }
+
+    @Bean
+    public Step step1(){
+        return stepBuilderFactory.get("step1")
+                .tasklet(helloWorldTasklet(null))
+                .build();
+    }
+
+    @Bean
+    public Job jobParametersJob(){
+        return jobBuilderFactory.get("jobParametersJob")
+                .start(step1())
+                .build();
+    }
+}
