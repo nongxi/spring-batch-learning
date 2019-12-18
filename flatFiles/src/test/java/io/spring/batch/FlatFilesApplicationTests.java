42d6793dc7875baf78c85f966907c376f5f0bed7
Banner Ironxi <bannerironxi@foxmail.com>
2019-12-19 03:56:48 +0800

    spring batch:read dat from csv file,chatper 9

---
+++ flatFiles/src/test/java/io/spring/batch/FlatFilesApplicationTests.java
@@ -0,0 +1,15 @@
+package io.spring.batch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlatFilesApplicationTestsBackup {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
+
+
