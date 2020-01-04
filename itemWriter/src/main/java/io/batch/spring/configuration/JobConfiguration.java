f8094db6649816de87adc88bfb5ab7297ed81dc9
nongxi <Ironxss@protonmail.com>
2020-01-05 00:29:48 +0800

    spring batch:itemWriter

---
+++ itemWriter/src/main/java/io/batch/spring/configuration/JobConfiguration.java
@@ -0,0 +1,54 @@
+package io.batch.spring.configuration;
+
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.item.ItemReader;
+import org.springframework.batch.item.support.ListItemReader;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Configuration
+public class JobConfiguration {
+    @Autowired
+    private JobBuilderFactory jobBuilderFactory;
+
+    @Autowired
+    private StepBuilderFactory stepBuilderFactory;
+
+    @Bean
+    public ItemReader<String> itemReader(){
+        List<String> items=new ArrayList<>(100);
+
+        for(int i=0;i<=100;i++){
+            items.add(String.valueOf(i));
+        }
+        return new ListItemReader<>(items);
+    }
+
+    @Bean
+    public SysOutItemWriter itemWriter(){
+        return new SysOutItemWriter();
+    }
+
+    @Bean
+    public Step step(){
+        return stepBuilderFactory.get("step")
+                .<String,String>chunk(10)
+                .reader(itemReader())
+                .writer(itemWriter())
+                .build();
+    }
+
+    @Bean
+    public Job job(){
+        return jobBuilderFactory.get("job")
+                .start(step())
+                .build();
+    }
+}
